1bfa5a9df549b5ae0df81f4bb5928df942ce561c
Colton Pense <flamingleos@gmail.com>
2021-06-20 12:25:48 -0500

    Source Files

---
+++ Assets/ColorinputBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Fall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        StartCoroutine(WaitForBody(Physics.RaycastAll(transform.position, input).OrderBy(hit => hit.distance).ToArray()));
+
+    }
+    IEnumerator WaitForBody(RaycastHit[] hits)
+    {
+        foreach (RaycastHit hit in hits)
+        {
+            yield return new WaitForSeconds(.4f);
+            hit.collider.gameObject.AddComponent<Rigidbody>();
+        }
+    }
+}
